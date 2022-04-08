fc5bd42a6f25c6f18c557d772812ef1732bb815b
ersidimatranxhi <ersidi.matranxhi1@gmail.com>
2022-04-08 17:42:13 +0200

    Add project files.

---
+++ CSharpShop/Prodotto.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpShop
+{
+    //CLASSE PRODOTTO ---------------
+    internal class Prodotto
+    {
+
+        // VARIABILI --------------
+        private int codiceProdotto;
+        private string nomeProdotto;
+        private string descrizione;
+        private double prezzo;
+        private string iva;
+
+        //---------- COSTRUTTORE --------------
+        public Prodotto(string nomeProdotto, string descrizione, double prezzo, string iva)
+        {
+
+            this.nomeProdotto = nomeProdotto;
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+
+        }
+
+        //-------------- METODO 1 CREAZIONE CODICE -------------
+
+        public int GetGeneraCodiceProdotto()
+        {
+
+            Random random = new Random();
+            int numero = random.Next(100);
+            return numero;
+
+
+        }
+
+        //------------ METODO 2 CALCOLA PREZZO CON IVA ------------
+
+        public double GetCalcolaPrezzoIva()
+        {
+
+            double prezzoConIva = prezzo * 1.22;
+            return prezzoConIva;
+
+        }
+
+        //----------- METODO 3 STAMPA NOME ESTESO ----------
+        public string GetNomeEsteso()
+        {
+
+            string nomePiuCodice = nomeProdotto + " " + GetGeneraCodiceProdotto();
+            return nomePiuCodice;
+
+        }
+
+        //------------ METODO 4 STAMPA DATI ---------------
+        public void StampaDati()
+        {
+
+            Console.WriteLine("BENVENUTI NEL NOSTRO E-COMMERCE" + "\n");
+            Console.WriteLine("------- I NOSTRI PRODOTTI -------");
+            Console.WriteLine("Prodotto: " + GetNomeEsteso());
+            Console.WriteLine("---------------------------------" + "\n");
+            Console.WriteLine("-----------DESCRIZIONE-----------");
+            Console.WriteLine(descrizione);
+            Console.WriteLine("---------------------------------" + "\n");
+            Console.WriteLine("-------------COSTI---------------");
+            Console.WriteLine("Prezzo senza iva: " + prezzo);
+            Console.WriteLine("Prezzo calcolato con iva: " + GetCalcolaPrezzoIva());
+            Console.WriteLine(".................................");
+            Console.WriteLine("\n" + "\n");
+
+
+        }
+
+    }
+}
---
+++ CSharpShop/Program.cs
@@ -0,0 +1,17 @@
+﻿using CsharpShop;
+
+Prodotto mioProdotto = new Prodotto("AirPods", "Cuffie wireless", 129.90, "22%");
+
+mioProdotto.GetGeneraCodiceProdotto();
+mioProdotto.GetCalcolaPrezzoIva();
+mioProdotto.GetNomeEsteso();
+mioProdotto.StampaDati();
+
+//----------- creo un altro oggetto ---------
+
+Prodotto mioProdotto2 = new Prodotto("Beats Studio",
+    "Cuffie con padiglione e con cavo", 250.90, "22%");
+mioProdotto2.GetGeneraCodiceProdotto();
+mioProdotto2.GetCalcolaPrezzoIva();
+mioProdotto2.GetNomeEsteso();
+mioProdotto2.StampaDati();
